ab5fc53488281e8132bccb73befe1a987e2fc952
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2018-12-26 17:57:14 -0500

    Update Challenging SQLZoo.sql

--- Challenging SQLZoo.sql
+++ Challenging SQLZoo.sql
@@ -2,6 +2,11 @@
 SELECT within SELECT Tutorial
 */
 
+/*
+list structure
+world(name, continent, area, population, gdp)
+*/
+
 --#1
 /*
 List each country name where the population is larger than that of 'Russia'.
